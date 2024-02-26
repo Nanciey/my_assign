bd89416aca9917e1d69aa4d991f0d01f4e7b1300
Nanciey <116234811+Nanciey@users.noreply.github.com>
2024-02-26 18:38:33 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,12 @@
+#Ask for user's name
+name = input("Enter your name: ")
+
+#ASk for user's age
+age = input("Enter your age: ")
+
+#Ask for user's location
+location = input("Enter your location: ")
+
+#Print out personalized message
+print("Hello {name}, you are {age} years old and live in {location}")
+
